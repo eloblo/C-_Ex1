7d15398ef0ed7a3eda9317af754c115c17026de9
Eli Belkind <eloblo250@gmail.com>
2021-03-07 11:15:42 +0200

    fix remove using std

--- snowman.hpp
+++ snowman.hpp
@@ -1,14 +1,13 @@
 #include <string>
-using namespace std;
 
 namespace ariel {
-    const string hat[] = {" _===_ \n", "  ___  \n ..... \n", "   _   \n  /_\\  \n", "  ___  \n (_*_) \n"};
-    const string base[] = {" ( : ) ", " (\" \") ", " (___) ", " (   ) "};
+    const std::string hat[] = {" _===_ \n", "  ___  \n ..... \n", "   _   \n  /_\\  \n", "  ___  \n (_*_) \n"};
+    const std::string base[] = {" ( : ) ", " (\" \") ", " (___) ", " (   ) "};
     const char eyes[] = {'.','o','O','-'};
     const char nose[] = {',','.','_',' '};
     const char Larm[] = {'<', '\\', '/', ' '};
     const char Rarm[] = {'>', '/', '\\', ' '};
-    const string body[] = {" ( : ) \n", " (] [) \n", " (> <) \n", " (   ) \n"};
-    const string head_tmp = " (   ) \n";
-    string snowman(long build);
+    const std::string body[] = {" ( : ) \n", " (] [) \n", " (> <) \n", " (   ) \n"};
+    const std::string head_tmp = " (   ) \n";
+    std::string snowman(long build);
 };
